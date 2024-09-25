77cee0c99ec31ea573e5dc6706b33b496cd0a2c3
cypartanoreply <no-reply@cyparta.com>
2024-09-25 22:59:39 +0300

    try

--- scripts/check_and_enable_ssl.sh
+++ scripts/check_and_enable_ssl.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-SSL_CERT="/etc/nginx/ssl/live/testmemo.cyparta.com/fullchain.pem"
-SSL_KEY="/etc/nginx/ssl/live/testmemo.cyparta.com/privkey.pem"
+DOMAIN=${DOMAIN_NAME}
+
+SSL_CERT="/etc/nginx/ssl/live/$DOMAIN/fullchain.pem"
+SSL_KEY="/etc/nginx/ssl/live/$DOMAIN/privkey.pem"
 SSL_CONF="/etc/nginx/conf.d/ssl.conf"
 SSL_TEMPLATE="/etc/nginx/conf.d/ssl.conf.template"
 
 # Check if both SSL certificate and key exist
 if [ -f "$SSL_CERT" ] && [ -f "$SSL_KEY" ]; then
+    echo "SSL certificate and key for $DOMAIN found. Applying configuration."
     # If both files exist, copy the SSL template to the active configuration
     cp "$SSL_TEMPLATE" "$SSL_CONF"
 else
+    echo "SSL certificate or key for $DOMAIN missing. Removing active SSL configuration."
     # If either file is missing, remove the active SSL configuration
     rm -f "$SSL_CONF"
 fi
